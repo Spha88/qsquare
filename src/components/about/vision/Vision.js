85f5951f9aefb0643bf898db0acdf9832155dee1
Siphamandla <mr.smehlomakulu@gmail.com>
2020-06-18 10:30:53 +0200

    Add new Vision and Value statements

--- src/components/about/vision/Vision.js
+++ src/components/about/vision/Vision.js
@@ -12,15 +12,15 @@ const Vision = () => {
                 </div>
                 <div className={classes.Card_body}>
                     <p>
-                        Reduce the virtual distance between businesses and
-                        clients through automated digital platforms.
+                        To help more people and businesses realise the full
+                        potential of the internet.
                     </p>
                 </div>
             </div>
             <div className={classes.Card}>
                 <div className={`${classes.Card_img} ${classes.Mission_img}`}>
                     <div className={classes.Card_header_container}>
-                        <h2>Mission</h2>
+                        <h2>Our Mission</h2>
                     </div>
                 </div>
                 <div className={classes.Card_body}>
