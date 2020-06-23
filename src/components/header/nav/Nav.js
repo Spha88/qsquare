d808d7ce44850c2773299933b3ab707c07fd644e
Siphamandla <mr.smehlomakulu@gmail.com>
2020-06-23 12:06:08 +0200

    Add scroll reveal to desktop nav

--- src/components/header/nav/Nav.js
+++ src/components/header/nav/Nav.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
 import classes from './Nav.module.scss';
 import BurgerIcon from '../BurgerIcon/BurgerIcon';
 import BurgerIconDesktop from '../BurgerIconDesktop/BurgerIcon';
 import MobileNav from '../MobileNav/MobileNav';
 import DesktopNav from '../DesktopNav/DesktopNav';
+
+import scrollReveal from 'scrollreveal';
 
 const Nav = () => {
     let [openMobileNav, setOpenMobileNav] = useState(false);
@@ -13,10 +15,23 @@ const Nav = () => {
     let [showDeskTopNav, setShowDeskTopNav] = useState(false);
     const toggleDesktopNav = () => setShowDeskTopNav(!showDeskTopNav);
 
+    useEffect(() => {
+        // ScrollReveal animation
+        const options = {
+            distance: '100%',
+            origin: 'top',
+            delay: '500',
+            duration: '1500',
+        };
+        scrollReveal().reveal('#nav-container', options);
+        //-------------------------------
+    }, []);
+
     return (
-        <div className={classes.NavContainer}>
+        <div className={classes.NavContainer} id='nav-container'>
             <nav className={classes.Nav}>
                 <div
+                    id='logo'
                     className={classes.Logo}
                     onClick={() => scroll.scrollToTop()}
                 ></div>
