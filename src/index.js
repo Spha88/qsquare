8d911494c17f768336da00f28a65bbdbbb630ae9
Siphamandla <mr.smehlomakulu@gmail.com>
2020-06-24 08:11:08 +0200

    Add react-app-polyfill for ie9

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
