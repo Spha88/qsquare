dfd6ef5d364b53e6bf541f56a367f2b3a3c1f77e
Siphamandla <mr.smehlomakulu@gmail.com>
2020-06-26 07:27:15 +0200

    Add scroll reveal effect to UpButtom

--- src/components/layout/UpButton/UpButton.js
+++ src/components/layout/UpButton/UpButton.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
 import classes from './UpButton.module.scss';
+import scrollReveal from 'scrollreveal';
 
 const UpButton = () => {
     // Handle hover effect to prevent flick and continue animation when the
@@ -13,8 +14,21 @@ const UpButton = () => {
         }, 400);
     };
 
+    useEffect(() => {
+        // ScrollReveal animation
+        const options = {
+            distance: '100%',
+            origin: 'right',
+            delay: '500',
+            duration: '1500',
+        };
+        scrollReveal().reveal('#up-button', options);
+        //-------------------------------
+    }, []);
+
     return (
         <div
+            id='up-button'
             className={`${classes.UpButtonContainer} ${hover && classes.hover}`}
             onClick={() => scroll.scrollToTop()}
             onMouseEnter={handleHover}
