1b70f612eac2cc4367c1de332f16bbd113728d00
Siphamandla <mr.smehlomakulu@gmail.com>
2020-06-26 06:47:33 +0200

    Add reacr-particles.js as background of Hero component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import classes from './Header.module.scss';
+import Particles from 'react-particles-js';
 
 import Nav from './nav/Nav';
 import Hero from './hero/Hero';
 
 const Header = () => {
     return (
-        <header className={classes.Header}>
+        <header className={classes.Header} id='header'>
             <Nav />
             <Hero />
+            <Particles
+                params={{
+                    particles: {
+                        number: {
+                            value: 200,
+                            density: {
+                                enable: true,
+                                value_area: 2000,
+                            },
+                        },
+                    },
+                }}
+            />
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
